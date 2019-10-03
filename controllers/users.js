1615b6e12998592036a67f0211c7296ffeddbaf7
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-10-03 07:44:08 -0700

    completed all routes and passing all 18 tests

--- controllers/users.js
+++ controllers/users.js
@@ -4,4 +4,16 @@ const router = express.Router();
 const User = require("../models/User");
 const Note = require("../models/Note");
 
+router.get("/", (req, res) => {
+  User.find({}).then(allUsers => {
+    res.json(allUsers);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  User.findOne({ _id: req.params.id }).then(user => {
+    res.json(user);
+  });
+});
+
 module.exports = router;
