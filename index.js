2aa55e410f42347c5d42e55e45cccdb1bf9a6f2e
Mike Smith <smithmichaels@johndeere.com>
2020-01-24 09:53:10 -0600

    Test

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const express = require("express");
+const parser = require("body-parser");
+const cors = require("cors");
+const bookmarksController = require("./controllers/notes");
+const usersController = require("./controllers/users");
+
+const app = express();
+
+app.use(parser.urlencoded({ extended: true }));
+
+app.use(parser.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.redirect("/api/users");
+});
+
+app.use("/api/users/", bookmarksController);
+
+app.use("/api/notes/", usersController);
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
