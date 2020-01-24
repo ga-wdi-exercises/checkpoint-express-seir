b69746e346c9bfd0b2ef2ad68bd1267fd41dbe42
unknown <raymondlevengood@gmail.com>
2020-01-24 11:00:56 -0500

    finished excersise

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const mongoose = require('../db/connection.js')
+const express = require('express');
+const parser = require("body-parser")
+const server = express();
+
+const notesController = require('./controllers/notes.js')
+const usersController = require('./controllers/users.js')
+
+server.use(parser.urlencoded({ extended: true }));
+server.use(parser.json());
+
+server.get('/', (req, res) => {
+    res.redirect("/notes");
+});
+
+server.use('./controllers/users.js', usersController)
+server.use('./controllers/notes.js', notesController)
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
