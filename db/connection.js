c10567fa94df49a201a57bb9d06bd7c68c6aee88
Grothusen <grothusenmichaelw@johndeere.com>
2020-01-24 10:02:23 -0600

    checkpoint

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/notes-checkpoint", {useNewUrlParser: true});
-
+const mongoURI = 'mongodb://localhost/notes-checpoint'
 mongoose.Promise = Promise;
+// mongoose.connect("mongodb://localhost/notes-checkpoint", {useNewUrlParser: true});
+
+mongoose
+  .connect(mongoURI, {useNewUrlParser: true})
+  .then(instance => console.log(`Connected to db: ${instance.connections[0].name}`))
+  .catch(error => console.log('Connection failed!', error))
+
 
 module.exports = mongoose;
