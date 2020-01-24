641c82e803891e9a89e609d7f32437157c475543
svkalvakolanu <skalvakolanu@gmail.com>
2020-01-24 10:19:41 -0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const express = require("express");
+const parser = require("body-parser");
+
+const app = express();
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+const notesController = require("./controllers/notes");
+const usersController = require("./controllers/users");
+
+app.use("/notes/", notesController);
+app.use("/users/", usersController);
+
+app.get("/", (req, res) => {
+  res.redirect("/notes/");
+});
+
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
