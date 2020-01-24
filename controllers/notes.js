bf7d75bd1ae5daca0203640d21e790d1658e2fee
magbee <david.magbee@me.com>
2020-01-24 10:51:52 -0500

    ...

--- controllers/notes.js
+++ controllers/notes.js
@@ -7,3 +7,6 @@ router.get('/', (req, res) => {
     Note.find().then(notes => res.json(notes));
 });
 
+router.get('/:id', (req, res) => {
+    Note.find().then(note => res.json(note));
+})
--- controllers/users.js
+++ controllers/users.js
@@ -6,3 +6,7 @@ const User = require('../models/User');
 router.get('/', (req, res) => {
     User.find().then(users => res.json(users));
 });
+
+router.get('/:id', (req, res) => {
+    User.find().then(user => res.json(user));
+})
