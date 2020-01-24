c0d20dcb38dceb3626ec445c52f61636b692c1e1
Jamrod <myfavoritenoisemaker@gmail.com>
2020-01-24 09:02:19 -0700

    most test passing

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-app.listen(3000, () => console.log('app is running'))
+const express = require('express')
+const app = express()
+
+const parser = require("body-parser");
+const cors = require("cors");
+
+const Note = require('./controllers/notes')
+const User = require('./controllers/users')
+
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.use(cors());
+
+app.use('/', Note)
+app.use('/users', User)
+
+app.listen(4000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
 module.exports = app
