0453849467970d4e0ccebfcc04dd0d3834e4c4a5
magbee <david.magbee@me.com>
2020-01-24 10:31:26 -0500

    up to step 5

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const notesController = require('./controllers/notes');
+const usersController = require('./controllers/users');
+
+const app = express();
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
--- models/User.js
+++ models/User.js
@@ -1,5 +1,14 @@
-const mongoose = require('../db/connection')
+const mongoose = require("../db/connection");
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+  username: String,
+  body: String,
+  notes: [
+    {
+      type: Schema.type.ObjectId,
+      ref: "Note"
+    }
+  ]
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
