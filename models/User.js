7afe816268c209ff8e7a3f89e0df5ff35527aad4
Jen Valladares <lilspikey333@yahoo.com>
2020-01-24 10:00:47 -0600

    why doesnt it recognize my Note model

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const app = require("../index.js");
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
-  notes: [Note.schema.obj]
+  notes: Note.schema.obj
 });
 
 module.exports = mongoose.model("User", userSchema);
